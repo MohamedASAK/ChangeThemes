d4b2a4682048999a1355ef394a70b40bcb8c030e
MohamedASAK <mohamedasak@gmail.com>
2023-08-30 10:43:18 +0300

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
+let bulbText = document.querySelector('.bulb label')
+let input = document.querySelectorAll("input");  
+let content = document.querySelectorAll("div");
 let button = document.querySelector("button");
 let body = document.querySelector("body");
 let bulb = document.querySelector("i");
-let bulbText = document.querySelector('.bulb label')
 
 bulb.addEventListener("click",()=>{
     if(!body.classList.contains("dark")) {
@@ -9,21 +11,30 @@ bulb.addEventListener("click",()=>{
         body.classList = ("dark"); 
         bulbText.innerText = 'Light Mode';
         body.style = '.dark';
+        for (let i = 0; i < content.length; i++) {
+            content[i].style = ".dark";
+        }
     }
     else{
         bulb.classList.replace("fa-regular","fa-solid");
         body.classList = ("light");
         bulbText.innerText = 'Dark Mode';
         body.style = '.light';
+        for (let i = 0; i < content.length; i++) {
+            content[i].style = ".dark";
+        }
     }
 }
 );
 
-let input = document.querySelectorAll("input");    
 button.addEventListener('click',()=>{
     let FontColor = input[0].value;
     let BackgroundColor = input[1].value;
-    body.style.backgroundColor = BackgroundColor;
+    let ContentBackgroundColor = input[2].value;
     body.style.color = FontColor;
+    body.style.backgroundColor = BackgroundColor;
+    for (let i = 0; i < content.length; i++) {
+        content[i].style.backgroundColor = ContentBackgroundColor;
+    }
 }
 );
